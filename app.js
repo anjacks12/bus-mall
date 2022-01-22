d38121b4ccf94759a93b30dae3561a637fafaac9
Angela Jackson <anjacks12@gmail.com>
2022-01-22 13:55:23 -0800

    made edits to match lab 12 code

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 // global variables
 const images = document.querySelector('section');
-//const results = document.getElementById('results');
+const results = document.getElementById('results');
 
 let itemsArray = [];
 let counter = 0;
-let maxClicks = 25; //needs to be changed to 25
+let maxClicks = 4; //needs to be changed to 25
 
 // array to hold 6 random items for display
 let randomNumberArray = [];
@@ -29,19 +29,19 @@ function randomItem() {
 // use random number to get images to appear randomly
 // got help from Andres from class with the repl.it example in class
 function renderItems() {
-  let randomNumberArray = [];
+  //let randomNumberArray = [];
   while (randomNumberArray.length < 6) {
     let item = randomItem();
     if (!randomNumberArray.includes(item)) {
       randomNumberArray.push(item);
     }
-    console.log(randomNumberArray);
+    //console.log(randomNumberArray);
   }
   for (let i = 0; i < randomNumberArray.length; i++) {
     let item1 = randomNumberArray.shift();
     let item2 = randomNumberArray.shift();
     let item3 = randomNumberArray.shift();
-    console.log('item1',item1,'item2',item2,'item3',item3);
+    //console.log('item1',item1,'item2',item2,'item3',item3);
     // // need to display 3 different items on page
     let image1 = document.getElementById('item1');
     image1.src = itemsArray[item1].src;
@@ -59,28 +59,6 @@ function renderItems() {
     itemsArray[item3].views++;
   }
   //console.log(randomNumberArray);
-
-  let item1 = randomNumberArray.shift();
-  let item2 = randomNumberArray.shift();
-  let item3 = randomNumberArray.shift();
-
-  //console.log('item1', item1, 'item2', item2, 'item3', item3);
-  // // need to display 3 different items on page
-  let image1 = document.getElementById('item1');
-  image1.src = itemsArray[item1].src;
-  image1.alt = itemsArray[item1].name;
-  let image2 = document.getElementById('item2');
-  image2.src = itemsArray[item2].src;
-  image2.alt = itemsArray[item2].name;
-  let image3 = document.getElementById('item3');
-  image3.src = itemsArray[item3].src;
-  image3.alt = itemsArray[item3].name;
-
-  //counting how many times the items were viewed
-  itemsArray[item1].views++;
-  itemsArray[item2].views++;
-  itemsArray[item3].views++;
-
 }
 
 //event handler for items
